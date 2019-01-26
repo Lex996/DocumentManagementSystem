e39b471482d94f654333f5ec8fe580cdce29094e
Lex996 <rakicaleksandra1@gmail.com>
2019-01-26 14:24:53 +0100

    Pokusaj

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+session_regenerate_id(TRUE);
+
+
+
+$con = new mysqli("localhost","root","", "mydb9eng");
+if (!$con)
+  {
+  die('Could not connect: '.mysql_error());
+  }
+
+
+$table='table2';
+
+//-----------------user level in members table not in use this is why I set admin here--
+$admin="admin";
+//-------------------------------------------------
+$title="Document Management System";
+
+?>
